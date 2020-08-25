4dc6243e35775d3278b43d830b2f59ce5bf4e762
hiteshK03 <1603coderz@gmail.com>
2020-08-25 17:42:42 +0530

    1_part_c

--- assignment-1-transformations/1/code/myMainScript.py
+++ assignment-1-transformations/1/code/myMainScript.py
@@ -20,6 +20,28 @@ def myBilinearInterpolation(img,P,Q):
             new[i-r+1:i,j] = np.linspace(new[i-r,j],new[i,j],r+1)[1:-1]
     return new
 
+def myNearestNeighborInterpolation(img, P, Q):
+    M, N = img.shape
+    new = np.zeros((P, Q))
+    
+    r,c = math.ceil(P/M), math.ceil(Q/N)
+    new[::r, ::c] = img
+    for j in range(0,Q,c):
+        for i in range(0,P-r,r):
+            new[i+1,j] = new[i,j]
+            new[i+r-1, j] = new[i+r,j]
+
+    for j in range(0, Q-c, c):
+        for i in range(0, P-r,r):
+            new[i+1,j+1] = new[i,j]
+            new[i+r-1, j+c-1] = new[i+r, j]
+    
+    for i in range(0,P, r):
+        for j in range(0,Q-c,c):
+            new[i, j+1] = new[i, j]
+
+    return new
+
 def imageWithColorbar(image,cmap='gray'):
     image = plt.imshow(image, cmap, interpolation = None)
     plt.colorbar()
@@ -40,6 +62,14 @@ def part_b(barbara):
     enlarged_barbara = myBilinearInterpolation(barbara,3*M-2,2*N-1)
     imageWithColorbar(enlarged_barbara)    
 
+def part_c(barbara):
+    ##################### Part(c) ########################
+    imageWithColorbar(barbara)
+    M,N = barbara.shape
+    enlarged_barbara = myNearestNeighborInterpolation(barbara,3*M-2,2*N-1)
+    imageWithColorbar(enlarged_barbara)    
+
+
 if __name__=='__main__':
         
     circles = mpimg.imread('../data/circles_concentric.png')
@@ -47,3 +77,4 @@ if __name__=='__main__':
 
     part_a(circles)
     part_b(barbara)
+    part_c(barbara)
