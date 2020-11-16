61d6c1aa682ac48d9c4a8275f4c1e6b9ba939e3f
hiteshK03 <1603coderz@gmail.com>
2020-11-16 22:57:55 +0530

    5-6 done

--- HW5/assignment5_DFT/5/code/myMainScript.m
+++ HW5/assignment5_DFT/5/code/myMainScript.m
@@ -1,6 +1,40 @@
 %% MyMainScript
 
 tic;
-%% Your code here
+I = zeros(300);
+I(50:100, 50:120) = 255;
+J = zeros(300);
+J(120:170, 20:90) = 255;
+fig1 = figure(1);
+subplot(121); imshow(I); title("Image I");
+subplot(122); imshow(J); title("Image J");
 
+f1 = fftshift(fft2(I)); f2 = fftshift(fft2(J));
+f = (f1 .* conj(f2))./(abs(f1 .* f2));
+lf = log(abs(f) + 1);
+fig2 = figure(2);
+imshow(lf,[-1 18]);
+colormap (jet); colorbar;
+title("log(|F(u,v)| + 1)");
+
+g = ifft2(f);
+fig3 = figure(3);
+imshow(g/max(g(:))); title("Shift needed on Image J for Restoration");
+
+I_noisy = I + randn(size(I)) * 20;
+J_noisy = J + randn(size(I)) * 20;
+fig4 = figure(4);
+subplot(121); imshow(I_noisy); title("Image I (Noisy)");
+subplot(122); imshow(J_noisy); title("Image J (Noisy)");
+
+f1_noisy = fftshift(fft2(I_noisy)); f2_noisy = fftshift(fft2(J_noisy));
+f_noisy = (f1_noisy .* conj(f2_noisy))./(abs(f1_noisy .* f2_noisy));
+lf_noisy = log(abs(f_noisy) + 1);
+fig5 = figure(5); 
+imshow(lf_noisy,[-1 18]);
+colormap (jet); colorbar; title("log(|F(u,v)| + 1)");
+
+g_noisy = ifft2(f_noisy);
+fig6 = figure(6);
+imshow(g_noisy/max(g_noisy(:))); title("Shift needed on Image J (Noisy) for Restoration");
 toc;
--- HW5/assignment5_DFT/6/code/myMainScript.m
+++ HW5/assignment5_DFT/6/code/myMainScript.m
@@ -1,6 +1,16 @@
 %% MyMainScript
 
 tic;
-%% Your code here
+k1 = [0,1,0; 1,-4,1; 0,1,0];
+k2 = [-1,-1,-1; -1,8,-1; -1,-1,-1];
 
+Fk1 = fftshift(fft2(k1,201,201));
+Fk2 = fftshift(fft2(k2,201,201));
+lf_k1 = log(abs(Fk1)+1);
+lf_k2 = log(abs(Fk2)+1);
+fig1 = figure(1);
+subplot(121); imshow(lf_k1,[-1 2]); colormap(jet); colorbar; 
+title('N,N-point DFT of k1');
+subplot(122); imshow(lf_k2,[-1 2]); colormap(jet); colorbar;
+title('N,N-point DFT of k2');
 toc;
